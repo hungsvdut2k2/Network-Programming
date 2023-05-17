0193f4efe8582aa743311dc41376b5cc45f9f74c
HitoriHellen <74436304+Hitori-Hellen@users.noreply.github.com>
2023-05-17 11:01:51 +0700

    fix DaoDirectory

---
+++ src/Dao/ConnectDatabase.java
@@ -0,0 +1,22 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectDatabase {
+    public static Connection getConnectDatabase() {
+        String url = "jdbc:mysql://localhost:3306/test";
+        String username = "root";
+        String password = "password";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            return connection;
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+        return null;
+    }
+//    public static void main(String[] args) {
+//        System.out.println(getConnectDatabase());
+//    }
+}
